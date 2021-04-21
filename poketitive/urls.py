996b38275c48fcba45670c4268f172bc8e02ef5d
Alejandro <aom54@alu.ua.es>
2021-04-21 16:54:40 +0200

    conf troubleshoot

--- poketitive/urls.py
+++ poketitive/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 from pokemoncompetitive import views
 
