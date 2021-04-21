6e3a8a241461a165d6f418fdd961cb8abad18bda
Alejandro <aom54@alu.ua.es>
2021-04-21 17:23:46 +0200

    Migration

--- poketitive/urls.py
+++ poketitive/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
 ]
 
 # urlpatterns += staticfiles_urlpatterns()
-urlpatterns += +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
